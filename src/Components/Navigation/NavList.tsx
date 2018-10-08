ba322fbfd8b917856b100390a6116263469fe839
Tyler Lartonoix <lord.codemonkey@gmail.com>
2018-10-08 15:42:31 -0400

    Remove components still dependent on MUI

--- src/Components/Navigation/NavList.tsx
+++
@@ -1,8 +0,0 @@
-import List from '@material-ui/core/List/List';
-import * as React from 'react';
-
-export const NavList: React.SFC<{}> = props => (
-	<List component="nav">
-		{props.children}
-	</List>
-);
--- src/Components/createLink.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import {Link, LinkProps} from 'react-router-dom';
-
-export const createLink = (path: string) => (props: LinkProps) => <Link to={path} {...props} />;
