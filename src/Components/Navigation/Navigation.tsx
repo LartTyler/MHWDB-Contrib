d016788e7589df66761fc90d1dfdb2d95c7aa654
Tyler Lartonoix <lord.codemonkey@gmail.com>
2019-02-12 13:24:36 -0500

    Add useful links to navbar

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -1,5 +1,17 @@
-import {Alignment, Button, Navbar, NavbarDivider, NavbarGroup, NavbarHeading} from '@blueprintjs/core';
+import {
+	Alignment,
+	Button,
+	Menu,
+	MenuDivider,
+	MenuItem,
+	Navbar,
+	NavbarDivider,
+	NavbarGroup,
+	NavbarHeading,
+	Popover,
+} from '@blueprintjs/core';
 import * as React from 'react';
+import {GoMarkGithub} from 'react-icons/go';
 import {Link} from 'react-router-dom';
 import {Logout} from '../Auth/Logout';
 import {ThemeSwitcher} from '../ThemeSwitcher';
@@ -20,6 +32,21 @@ export const Navigation: React.FC<{}> = () => (
 		</NavbarGroup>
 
 		<NavbarGroup align={Alignment.RIGHT}>
+			<Popover>
+				<Button icon={<GoMarkGithub size={16} />} minimal={true} style={{marginRight: 5}} />
+
+				<Menu>
+					<MenuDivider title="Visit us on GitHub!" />
+					<MenuItem href="https://github.com/LartTyler/MHWDB-API" text="API" />
+					<MenuItem href="https://github.com/LartTyler/MHWDB-Docs/issues" text="Issues / Suggestions" />
+					<MenuItem href="https://github.com/LartTyler/MHWDB-Contrib" text="Contrib (this site)" />
+
+					<MenuDivider />
+
+					<MenuItem href="https://docs.mhw-db.com" text="API Documentation" />
+				</Menu>
+			</Popover>
+
 			<div style={{marginRight: 5}}>
 				<ThemeSwitcher />
 			</div>
