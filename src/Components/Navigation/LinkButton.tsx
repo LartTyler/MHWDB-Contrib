9f20af6afdd478e5e314cf4ac42e21315862d9b1
Tyler Lartonoix <lord.codemonkey@gmail.com>
2019-04-13 22:23:25 -0400

    Add from to link state

--- src/Components/Navigation/LinkButton.tsx
+++ src/Components/Navigation/LinkButton.tsx
@@ -2,6 +2,7 @@ import {Button, IButtonProps} from '@blueprintjs/core';
 import * as React from 'react';
 import {Link, LinkProps} from 'react-router-dom';
 import {Omit} from 'utility-types';
+import {history} from '../../history';
 
 interface ILinkButtonProps {
 	to: string;
@@ -11,7 +12,16 @@ interface ILinkButtonProps {
 }
 
 export const LinkButton: React.FC<ILinkButtonProps> = props => (
-	<Link to={props.to} {...props.linkProps} className={props.linkProps.className || 'plain-link'}>
+	<Link
+		to={{
+			pathname: props.to,
+			state: {
+				from: history.location.pathname,
+			},
+		}}
+		{...props.linkProps}
+		className={props.linkProps.className || 'plain-link'}
+	>
 		<Button {...props.buttonProps}>
 			{props.children}
 		</Button>
